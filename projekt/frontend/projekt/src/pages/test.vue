7514e525a1212ba673a6f90a2b1ae39620fba5ec
karolina444 <49405127+karolina444@users.noreply.github.com>
2019-05-14 19:56:26 +0200

    14-05

---
+++ projekt/frontend/projekt/src/pages/test.vue
@@ -0,0 +1,31 @@
+<template>
+<q-page padding>
+ <div class="row q-gutter-xs">
+      <div class="col">
+        jedan
+        <div>
+        <q-btn color="primary" label="Primary" @click="handleClick" />
+        <q-input outlined v-model="myInput" label="Outlined" />
+        </div>
+      </div>
+      <div class="col">
+        dva
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      myInput: ''
+    }
+  },
+  methods: {
+    handleClick () {
+      this.myInput = 'clicked'
+    }
+  }
+}
+</script>
